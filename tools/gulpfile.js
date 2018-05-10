9e7a380efb97a42c18b4a09dafc4b96a8fff723a
x0a <x0a.cake@gmail.com>
2018-05-10 12:39:53 -0500

    added source mapping

--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -1,7 +1,9 @@
 let gulp = require("gulp");
 let cleanCSS = require("gulp-clean-css");
 let pug = require("gulp-pug");
+let concat = require("gulp-concat");
 let minify = require("gulp-uglify-es").default;
+let sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("minify-css", () => {
 	return gulp.src("../src/*.css")
@@ -17,7 +19,10 @@ gulp.task("pug", () => {
 
 gulp.task("js", () => {
 	return gulp.src("../src/*.js")
+	.pipe(sourcemaps.init())
+	.pipe(concat("index.js"))
 	.pipe(minify())
+	.pipe(sourcemaps.write())
 	.pipe(gulp.dest("../dist"))
 })
 
