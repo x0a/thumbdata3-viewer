8fc5b62ce0dcf647b0edaafed7fbc588eefce442
x0a <x0a.cake@gmail.com>
2018-05-13 12:49:58 -0500

    move to root, add package data

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,27 @@ let cleanCSS = require("gulp-clean-css");
 let pug = require("gulp-pug");
 let concat = require("gulp-concat");
 let minify = require("gulp-uglify-es").default;
+let ts = require("gulp-typescript");
 let sourcemaps = require("gulp-sourcemaps");
 
+let tsProject = ts.createProject("tsconfig.json");
+
 gulp.task("minify-css", () => {
-	return gulp.src("../src/*.css")
+	return gulp.src("src/*.css")
 	.pipe(cleanCSS())
-	.pipe(gulp.dest("../dist"))
+	.pipe(gulp.dest("dist/"))
 })
 
 gulp.task("pug", () => {
-	return gulp.src("../src/*.pug")
+	return gulp.src("src/*.pug")
 	.pipe(pug())
-	.pipe(gulp.dest("../dist/"))
+	.pipe(gulp.dest("dist/"))
 })
 
 gulp.task("js", () => {
-	return gulp.src("../src/*.js")
-	.pipe(sourcemaps.init())
-	.pipe(concat("index.js"))
-	.pipe(minify())
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest("../dist"))
+	return tsProject.src()
+	.pipe(tsProject())
+	.js.pipe(gulp.dest("dist/"))
 })
 
 gulp.task("default", gulp.parallel("minify-css", "pug", "js"))
