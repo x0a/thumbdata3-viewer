a4c9c0c7fa794534c20958b6590e864ee39618db
x0a <x0a.cake@gmail.com>
2018-05-13 12:51:29 -0500

    switch to typescript, move to plain js, ditch angular

---
+++ src/index.ts
@@ -0,0 +1,93 @@
+interface DOMObject{
+	fileOpen: HTMLButtonElement;
+	filePicker: HTMLInputElement;
+	imageViewer: HTMLDivElement;
+	closeButton: HTMLButtonElement;
+	imageContainer: HTMLDivElement;
+	emptyText: HTMLDivElement;
+	imageTemplate: HTMLDivElement;
+	imageViewerMain: HTMLImageElement;
+}
+
+((window: Window, document: Document, console: Console) => {
+	window.addEventListener("DOMContentLoaded", () => {
+
+		let DOM: DOMObject = {
+			fileOpen: document.querySelector("#filepickerOpen"),
+			filePicker: document.querySelector("#filepicker"),
+			imageContainer: document.querySelector("#imageContainer"),
+			emptyText: document.querySelector("#emptyText"),
+			imageTemplate: document.querySelector("#imageTemplate"),
+			imageViewer: document.querySelector(".image-viewer"),
+			imageViewerMain: document.querySelector(".image-viewer img"),
+			closeButton: document.querySelector("#closeButton"),
+		}
+
+		let updateList = (images: string[]) => {
+			let children: NodeListOf<HTMLDivElement> = DOM.imageContainer.querySelectorAll(".col-md-3:not(.d-none)");
+			children.forEach(child => DOM.imageContainer.removeChild(child))
+
+			if(images.length === 0){
+				DOM.emptyText.classList.remove("d-none");
+			}else{
+				DOM.emptyText.classList.add("d-none");
+
+				for(let i:number = 0; i < images.length; i++){
+					let imageChild = <HTMLDivElement> DOM.imageTemplate.cloneNode(true);
+					let imagePreview: HTMLImageElement = imageChild.querySelector("img");
+
+					imageChild.classList.remove("d-none");
+					imagePreview.src = images[i];
+
+					imageChild.addEventListener("click", () => {
+						DOM.imageViewerMain.src = images[i];
+						DOM.imageViewer.classList.remove("d-none");
+					})
+
+					DOM.imageContainer.appendChild(imageChild);
+				}
+			}
+		}
+
+		DOM.filePicker.addEventListener("change", (event:Event) => {
+			let target = (<HTMLInputElement> event.target);
+			if(target.files.length === 0) return;
+
+			let file:File = target.files[0];
+			let reader:FileReader = new FileReader();
+
+			reader.addEventListener("loadend", () => {
+				let data: Uint8Array = new Uint8Array(reader.result);
+				let markerStart: boolean = false;
+				let jpegStart: number = 0;
+				let images: string[] = [];
+
+				for(let i = 0; i < data.length; i++){
+					if(data[i] === 0xff && !markerStart){
+						markerStart = true;
+					}else if(markerStart){
+						if(data[i] === 0xd8)
+							jpegStart = i - 1;
+						else if(data[i] === 0xd9)
+							images.push(URL.createObjectURL(new Blob([data.slice(jpegStart, i)], {type: "image/jpeg"})));
+						
+						markerStart = false;
+					}
+				}
+				updateList(images);
+			})
+
+			reader.readAsArrayBuffer(file);
+		})
+
+		DOM.fileOpen.addEventListener("click", () => {
+			DOM.filePicker.click();
+		})
+
+		DOM.closeButton.addEventListener("click", () => {
+			DOM.imageViewer.classList.add("d-none");
+		})
+
+		
+	})
+})(window, document, console)
